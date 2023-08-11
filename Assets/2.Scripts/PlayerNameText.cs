3d98ca0049e594cdcb97cb39e172353f3748e93d
KwonBoWon <bogod1207@naver.com>
2023-08-12 03:27:18 +0900

    pingpong

---
+++ Assets/2.Scripts/PlayerNameText.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerNameText : MonoBehaviour
+{
+    private Text nameText;
+
+    private void Start()
+    {
+        nameText = GetComponent<Text>();
+
+    }
+}
